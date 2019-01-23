72c54abef2407236589eed8ff9296ac2072c41bc
Alyssa Brown <alyssa.brown@live.com>
2019-01-23 01:38:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,13 @@ def reverse_each_word(string)
   string.split("")
   
 end 
+
+def reverse_each_word(sentence1)
+  new_array = []
+  sentence = sentence1.split(" ")
+  sentence.each do |word|
+  new_array<< word.split("").reverse.join("")
+ # new_array.split(" ") So we do not want to call this here inside the iteration.
+  end
+  new_array.join(" ") #we want to call it here because here it's where we are returning it
+end
